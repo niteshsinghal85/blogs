47dd021cb48c61855f84cf6c840167a41e6710a5
Nitesh Singhal <40641126+niteshsinghal85@users.noreply.github.com>
2021-09-16 10:41:47 +0530

    Add files via upload

---
+++ GenericRepositoryPattern/Repository/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using GenericRepositoryPattern.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GenericRepositoryPattern.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+    }
+}
---
+++ GenericRepositoryPattern/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using GenericRepositoryPattern.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace GenericRepositoryPattern.Repository
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        void Add(T entity);
+        Task<bool> SaveChangesAsync();
+        Task<T> FindByConditionAsync(Expression<Func<T, bool>> predicate);
+    }
+}
