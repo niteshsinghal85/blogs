b884f3463e09e57f7b5e7ff5af5d164326fd1360
Nitesh <niteshsinghal85@gmail.com>
2021-11-09 12:30:59 +0530

    Corrected dir structure

---
+++ GenericRepositoryPattern/GenericRepositoryPattern/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenericRepositoryPattern.Entities
+{
+    public class BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
---
+++ GenericRepositoryPattern/GenericRepositoryPattern/Repository/Repository.cs
@@ -0,0 +1,48 @@
+﻿using GenericRepositoryPattern.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace GenericRepositoryPattern.Repository
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<T> _entities;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _entities = context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _entities.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _entities.SingleOrDefaultAsync(s => s.Id == id);
+        }
+
+        public void Add(T entity)
+        {
+            _entities.Add(entity);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync().ConfigureAwait(false) > 0;
+        }
+
+        public async Task<T> FindByConditionAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().FirstOrDefaultAsync(predicate);
+        }
+
+    }
+}
